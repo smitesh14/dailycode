bc44ffb30f7dd68a1fa2fa7f452a8dda000fc0de
smitesh14 <130777154+smitesh14@users.noreply.github.com>
2023-04-16 00:25:35 +0530

    Add files via upload
    15/04

---
+++ april/15-majority-element-n-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> v;
+        map<int,int> m;
+        for(auto x:nums)
+            m[x]++;
+        for(auto x:m){
+            if(x.second>floor(n/3)){
+                v.push_back(x.first);
+            }
+        }
+        return v;
+    }
+};
